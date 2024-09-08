fad2e3e4b3322a1ec2641c5b2bd7342c72c41529
Winn Z <winnwyz@gmail.com>
2024-09-08 21:25:56 +1000

    Add an api to create a note

--- infra/api.ts
+++ infra/api.ts
@@ -1,8 +1,14 @@
-import { bucket } from "./storage";
+import { table } from "./storage";
 
-export const api = new sst.aws.ApiGatewayV2("Api");
-
-api.route("GET /", {
-  link: [bucket],
-  handler: "packages/functions/src/api.handler",
+// Create the API
+export const api = new sst.aws.ApiGatewayV2("Api", {
+  transform: {
+    route: {
+      handler: {
+        link: [table],
+      },
+    }
+  }
 });
+
+api.route("POST /notes", "packages/functions/src/create.main");
