3cb63d2eefd95c605671908d7dcccf479249845d
Winn Z <winnwyz@gmail.com>
2025-01-23 19:02:37 +1100

    Add an api to delete a note

--- infra/api.ts
+++ infra/api.ts
@@ -15,3 +15,4 @@ api.route("POST /notes", "packages/functions/src/create.main");
 api.route("GET /notes/{id}", "packages/functions/src/get.main");
 api.route("GET /notes", "packages/functions/src/list.main");
 api.route("PUT /notes/{id}", "packages/functions/src/update.main");
+api.route("DELETE /notes/{id}", "packages/functions/src/delete.main");
