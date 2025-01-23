da19b6d8040f05c799771a062ce17face9748145
Winn Z <winnwyz@gmail.com>
2025-01-23 16:51:24 +1100

    Add an api to get all notes

--- infra/api.ts
+++ infra/api.ts
@@ -13,3 +13,4 @@ export const api = new sst.aws.ApiGatewayV2("Api", {
 
 api.route("POST /notes", "packages/functions/src/create.main")
 api.route("GET /notes/{id}", "packages/functions/src/get.main")
+api.route("GET /notes", "packages/functions/src/list.main")
