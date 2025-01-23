d2f69ee3ad12ea16d7cad056a3b8cea6d173fbe3
Winn Z <winnwyz@gmail.com>
2025-01-23 14:17:54 +1100

    Adding storage

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,1 +1,10 @@
+// Create an S3 bucket
 export const bucket = new sst.aws.Bucket("Uploads");
+
+export const table = new sst.aws.Dynamo("Notes", {
+  fields: {
+    userId: "string",
+    noteId: "string",
+  },
+  primaryIndex: { hashKey: "userId", rangeKey: "noteId" },
+});
