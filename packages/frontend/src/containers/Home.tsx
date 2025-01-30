9378aff78aeb60cf1a8f75058e415ad0e373218a
Winn Z <winnwyz@gmail.com>
2025-01-31 10:01:55 +1100

    List notes and edit a note

--- packages/frontend/src/containers/Home.tsx
+++ packages/frontend/src/containers/Home.tsx
@@ -1,12 +1,90 @@
+import { useEffect, useState } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useAppContext } from "../lib/contextLib";
 import "./Home.css";
+import { NoteType } from "../types/note";
+import { API } from "aws-amplify";
+import { onError } from "../lib/errorLib";
+import { Link } from "react-router-dom";
+import { BsPencilSquare } from "react-icons/bs";
 
 export default function Home() {
+  const [notes, setNotes] = useState<Array<NoteType>>([]);
+  const { isAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function onLoad() {
+      if (!isAuthenticated) {
+        return;
+      }
+
+      try {
+        const notes = await loadNotes();
+        setNotes(notes);
+      } catch (e) {
+        onError(e);
+      }
+
+      setIsLoading(false);
+    }
+
+    onLoad();
+  }, [isAuthenticated]);
+
+  function loadNotes() {
+    return API.get("notes", "/notes", {});
+  }
+
+  function formatDate(str: undefined | string) {
+    return !str ? "" : new Date(str).toLocaleString();
+  }
+
+  function renderNotesList(notes: NoteType[]) {
     return (
-        <div className="Home">
-            <div className="lander">
-                <h1>Scratch</h1>
-                <p className="text-muted">A simple note taking app</p>
-            </div>
-        </div>
-    )
+      <>
+        <Link to="/notes/new">
+          <ListGroup.Item action className="py-3 text-nowrap text-truncate">
+            <BsPencilSquare size={17} />
+            <span className="ms-2 fw-bold">Create a new note</span>
+          </ListGroup.Item>
+        </Link>
+        {notes.map(({ noteId, content, createdAt }) => (
+          <Link key={noteId} to={`/notes/${noteId}`}>
+            <ListGroup.Item action className="text-nowrap text-truncate">
+              <span className="fw-bold">{content.trim().split("\n")[0]}</span>
+              <br />
+              <span className="text-muted">
+                Created: {formatDate(createdAt)}
+              </span>
+            </ListGroup.Item>
+          </Link>
+        ))}
+      </>
+    );
+  }
+
+  function renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p className="text-muted">A simple note taking app</p>
+      </div>
+    );
+  }
+
+  function renderNotes() {
+    return (
+      <div className="notes">
+        <h2 className="pb-3 mt-4 mb-3 border-bottom">Your Notes</h2>
+        <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
+      </div>
+    );
+  }
+
+  return (
+    <div className="Home">
+      {isAuthenticated ? renderNotes() : renderLander()}
+    </div>
+  );
 }
