d4f096334ca6b315c80696f66c28932ae22a1ddc
Winn Z <winnwyz@gmail.com>
2025-01-30 15:38:41 +1100

    Sign up a user

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home.tsx";
 import NotFound from "./containers/NotFound.tsx";
 import Login from "./containers/Login.tsx";
+import Signup from "./containers/Signup.tsx";
 
 export default function Links() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/>
-            {/* Finally, catch all unmatched routes */}
-            <Route path="*" element={<NotFound/>}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      {/* Finally, catch all unmatched routes */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
